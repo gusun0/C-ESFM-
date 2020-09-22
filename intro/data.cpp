eba7c47374d742339f2aad08546f93eac16de7b2
abraham <fpatan@mail.ru>
2020-09-22 12:10:53 -0500

    1

---
+++ intro/data.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int numero = 10.5;
+	float real = 0.0000000000000000000000000000000000000000000000021;
+	char caracter = 'a';
+
+	cout << "entero: " << numero << endl; 
+	cout << "float: " << real << endl; 
+	cout << "caracter " << caracter << endl; 
+
+
+	unsigned int entero = 1;
+	cout << entero << endl;
+	
+	// PARA GUARDAR DATOS DE > VALOR
+	long long int entero_grande = 1000000000000000000;
+	cout << entero_grande << endl;
+
+	double doble = 0.0000000000000000000000000000000000000000000000021;
+	cout << doble << endl;
+
+
+	bool booleano = true;
+	cout << booleano << endl;
+	
+	
+	int n = 10;
+	cout << n << endl;
+	n = 20;
+	cout << n << endl;
+
+	int num;
+	cin >> num; cout << num << endl;
+
+	char letra;
+	cin >> letra; cout << letra << endl;
+
+
+	return 0;
+}
