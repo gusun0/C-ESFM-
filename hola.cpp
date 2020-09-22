0e1f3915804eeef7911526e0792f8d754850dbda
abraham <fpatan@mail.ru>
2020-09-22 11:18:42 -0500

    h

---
+++ hola.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	// lineas optimizadoras
+	
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0);
+
+	// lineas optimizadoras
+
+	cout << "Hola" << endl;
+	
+	return 0;
+}
+
+
+
+
+
+
